9c0cf678c9478b913caa1130c5ff20a53ad9f2f5
Francisco Bernal Yescas <francisco@sproutling.com>
2017-07-04 01:52:59 -0700

    Access-Control-Allow-Origin

--- index.php
+++ index.php
@@ -3,4 +3,5 @@
   header('Access-Control-Request-Method: *');
   header('Access-Control-Allow-Headers: *');
   header( 'Location: /index.html');
+  header('Access-Control-Allow-Origin: https://cit261-byui-fby.herokuapp.com', false);
 ?>
